1bee769f80bb2deb564cb492ac978c57ad479763
Amelia V <amelianvance@gmail.com>
2020-07-15 17:03:13 -0400

    Updates

---
+++ precache-manifest.77e676066bcb652d6abdde60ab7e591f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198fa7448b90529e56cd7303081016f7",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "436b678c71289b5cd067",
+    "url": "/TodoList/static/js/2.135a044c.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/TodoList/static/js/2.135a044c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee7cef5b2028f39e81f",
+    "url": "/TodoList/static/js/main.446eb5cf.chunk.js"
+  },
+  {
+    "revision": "ed1d86387aeaa15b734a",
+    "url": "/TodoList/static/js/runtime-main.c819b122.js"
+  }
+]);
